5abc0e8feddecb75aea6d1430db52f2a820fbfd6
osama alamer <osama.alamer@hotmail.com>
2021-12-27 21:13:54 +0000

    changes made from theia lab environment

--- sentimentAnalyzeClient/src/App.js
+++ sentimentAnalyzeClient/src/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
   value of the state, will be returned. The initial input mode
   is set to text
   */
+ 
   state = {innercomp:<textarea rows="4" cols="50" id="textinput"/>,
             mode: "text",
           sentimentOutput:[],
@@ -21,7 +22,10 @@ class App extends React.Component {
   If the requested input mode is "text" it returns a textbox with 4 rows.
   If the requested input mode is "url" it returns a textbox with 1 row.
   */
- 
+ componentDidMount() {
+    document.title = "Sentiment Analyzer"; 
+ }
+
   renderOutput = (input_mode)=>{
     let rows = 1
     let mode = "url"
@@ -49,9 +53,9 @@ class App extends React.Component {
         let output = data.label;
         let color = "white"
         switch(output) {
-          case "positive": color = "black";break;
-          case "negative": color = "black";break;
-          default: color = "black";
+          case "positive": color = "green";break;
+          case "negative": color = "red";break;
+          default: color = "yellow";
         }
         output = <div style={{color:color,fontSize:20}}>{output}</div>
         this.setState({sentimentOutput:output});
@@ -75,6 +79,7 @@ class App extends React.Component {
   render() {
     return (  
       <div className="App">
+          
       <button className="btn btn-info" onClick={()=>{this.renderOutput('text')}}>Text</button>
         <button className="btn btn-dark"  onClick={()=>{this.renderOutput('url')}}>URL</button>
         <br/><br/>
